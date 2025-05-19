e8c212081a85fac8005724a526c41157f35f4581
Johnson Wang <yuhang0229@gmail.com>
2025-05-19 13:26:13 +1000

    squiggle not for safari

--- src/slices/Hero/WavyPaths.tsx
+++ src/slices/Hero/WavyPaths.tsx
@@ -5,10 +5,13 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import clsx from "clsx";
 
+import { useIsSafari } from "@/lib/useIsSafari";
+
 gsap.registerPlugin(useGSAP);
 
 export function WavyPaths() {
   const root = useRef<SVGSVGElement>(null);
+  const isSafari = useIsSafari(true);
 
   useGSAP(() => {
     if (!root.current) return;
@@ -36,7 +39,10 @@ export function WavyPaths() {
       viewBox="0 0 1242 308"
       width={1242}
       height={308}
-      className={clsx("pointer-events-none text-zinc-600 animate-squiggle")}
+      className={clsx(
+        "pointer-events-none text-zinc-600",
+        !isSafari && "animate-squiggle"
+      )}
     >
       <path
         stroke="currentColor"
